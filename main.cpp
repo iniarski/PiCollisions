6b5e9e45c164da541788539d520fe50d5f3e4ef2
iniarski <filipmw2002@gmail.com>
2022-07-25 10:21:19 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+// Two blocks with mass ratio 1 : 100 ^ n (n being natural number),
+// the bigger block having initial velocity v,
+// collide perfectly elasticaly with eachother, and immovable wall
+// on a frictionless surface
+
+// The number of total collision will spell out the begiging n decimal places of the number Pi
+
+// For example, for mass ratio 1 : 10 000 (100^2)
+// the total numer of collisions will be 314 as in 3,141592...
+// (after that the bigger block will move towards rigth with greater velocity than the smaller one)
+
+//   | 
+//   | 
+//   | 
+// W | 
+// A | 
+// L |                         |===============|
+// L |                         |               |
+//   |                    _    |               |
+//   |                    V    |  M = 100 ^ n  |
+//   |   |=====|      <======= |               |
+//   |   |  m  |               |               |
+//   |   |=====|               |               |
+// ==========================================================================
+
+// Using the perfecty elastic collision formula
+
+//                (m1 - m2) * v1 + 2 * m2 * v2
+//          Vf =  ----------------------------
+//                          m1 + m2
+
+// We can approximate the number Pi
+
+// At about n = 11 the floating point arithmetics make the result differ from actual number Pi
+
+// Also the algorith is painfully inefficient having O(n) = 10 ^ n complexity
+// (only a gimmick, not for real applications)
+
+double pi(int digits){
+
+	// Initial parameters
+
+	double smalMass = 1;
+	double biggMass = pow(100, digits);
+	double smalVel = 0;
+	double biggVel = -16;
+
+	double totlMass = smalMass + biggMass;
+
+	double colCount = 0;
+
+	while(!(smalVel < biggVel && smalVel >= 0)){ // *the end condition
+		// the bigger block is moving rigth faster than the smaller one
+
+		if(smalVel < 0){
+			// Small block hitting the wall
+			// and bouncing back with equal speed
+			smalVel = - smalVel;
+		}
+		else{
+			// The two blocks colliding
+			double tempVel = ((smalMass - biggMass) * smalVel + 2 * biggMass * biggVel) / totlMass;
+			biggVel = ((biggMass - smalMass) * biggVel + 2 * smalMass * smalVel) / totlMass;
+
+			smalVel = tempVel;
+		}
+		colCount++;
+	}
+	return colCount / pow(10, digits);
+}
+
+int main(){
+	
+
+	for(int i = 0; i <= 10; i ++){
+		// Setting the number of decimal places
+		std :: cout << std :: fixed << std :: setprecision(i);
+		
+		std :: cout << i << " digits of Pi calculated using the colliding blocks method : " << std :: endl;
+		std :: cout << pi(i) << std :: endl;
+
+	}
+
+	return 0;
+}
